0dee9b4d12d83128a70e4beb45895cb6c88499a1
edveloso <edveloso@gmail.com>
2022-12-05 19:55:21 -0300

    atualizacao das apis

--- ecommece/src/main/java/br/edu/infnet/ecommerce/clients/ProdutoClient.java
+++ ecommece/src/main/java/br/edu/infnet/ecommerce/clients/ProdutoClient.java
@@ -4,14 +4,14 @@ import java.util.List;
 
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 
 import br.edu.infnet.ecommerce.resources.dto.ProdutoCatalogoDTO;
 
 @FeignClient("produto")
 public interface ProdutoClient {
 
-    @RequestMapping("/produtos")
+    @GetMapping("/produtos")
     ResponseEntity<List<ProdutoCatalogoDTO>> getProdutos();
     
 }
--- ecommece/src/main/java/br/edu/infnet/ecommerce/resources/PedidoResource.java
+++ ecommece/src/main/java/br/edu/infnet/ecommerce/resources/PedidoResource.java
@@ -14,6 +14,7 @@ import org.springframework.web.client.RestTemplate;
 import br.edu.infnet.ecommerce.clients.ProdutoClient;
 import br.edu.infnet.ecommerce.resources.dto.ClienteDTO;
 import br.edu.infnet.ecommerce.resources.dto.PedidoDTO;
+import br.edu.infnet.ecommerce.resources.dto.PedidoResponseDTO;
 import br.edu.infnet.ecommerce.resources.dto.ProdutoCatalogoDTO;
 
 @RestController
@@ -30,7 +31,7 @@ public class PedidoResource {
 	private ProdutoClient produtoClient;
 	
 	@PostMapping
-	public void efetuaPedido(@RequestBody PedidoDTO pedidoDTO) {
+	public PedidoResponseDTO efetuaPedido(@RequestBody PedidoDTO pedidoDTO) {
 		
 		ClienteDTO clienteDTO = restTemplate.getForObject(clienteApiUrl+
 					pedidoDTO.getClienteId(), ClienteDTO.class);
@@ -39,6 +40,8 @@ public class PedidoResource {
 		
 		ResponseEntity<List<ProdutoCatalogoDTO>> produtos = produtoClient.getProdutos();
 		System.out.println("foi " + produtos.getBody());
+		
+		return new PedidoResponseDTO(clienteDTO, produtos.getBody());
 		
 	}
 

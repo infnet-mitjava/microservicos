94c86a113232b430d7f927b23a27128948b9e544
edveloso <edveloso@gmail.com>
2022-12-12 19:20:44 -0300

    circuit break

--- gateway/src/main/java/br/edu/infnet/gateway/GatewayApplication.java
+++ gateway/src/main/java/br/edu/infnet/gateway/GatewayApplication.java
@@ -5,7 +5,12 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.gateway.route.RouteLocator;
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.context.annotation.Bean;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+import reactor.core.publisher.Mono;
+
+@RestController
 @SpringBootApplication
 public class GatewayApplication {
 
@@ -13,15 +18,28 @@ public class GatewayApplication {
 	public RouteLocator myRoutes(RouteLocatorBuilder builder) {
 		return builder.routes()
 				.route(p -> p
-					.path("/get")
-					.filters(f ->f.addRequestHeader("Hello", "World"))
-					.uri("http://httpbin.org:80")
+					.path("/clientes/**")
+//					.filters(f ->f.addRequestHeader("Hello", "World"))
+					.uri("http://localhost:8989")
 						)
+				.route(p -> p
+						.path("/produtos/**")
+						.filters(f ->f.circuitBreaker(config -> config
+								.setName("mycmd")
+								.setFallbackUri("forward:/fallback"))
+								)
+						.uri("http://localhost:8282")
+							)
 				.build();
 	}
 	
 	public static void main(String[] args) {
 		SpringApplication.run(GatewayApplication.class, args);
+	}
+	
+	@RequestMapping("/fallback")
+	public Mono<String> fallback() {
+		return Mono.just("fallback");
 	}
 
 }

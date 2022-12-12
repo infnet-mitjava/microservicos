c027864717209dd8b0240e4151beb43156a3725e
edveloso <edveloso@gmail.com>
2022-12-12 18:37:22 -0300

    inclusao do gateway

--- clientes/src/main/java/br/edu/infnet/clientes/ClientesApplication.java
+++ clientes/src/main/java/br/edu/infnet/clientes/ClientesApplication.java
@@ -2,10 +2,12 @@ package br.edu.infnet.clientes;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
 @EnableEurekaClient
+@EnableDiscoveryClient
 public class ClientesApplication {
 
 	public static void main(String[] args) {
